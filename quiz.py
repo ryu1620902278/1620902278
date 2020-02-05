11f39379a93ac7bc971f53b571e2ff0458566ad6
ryu1620902278 <60685744+ryu1620902278@users.noreply.github.com>
2020-02-05 14:51:18 +0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,3 +2,15 @@ msg = "My name is jirapong"
 print(msg)
 msg = "lsdkjgnlsngl"
 print(msg)S
+msg = "My name is jirapong"
+print(msg)
+msg = "lsdkjgnlsngl"
+print(msg)S
+msg = "My name is jirapong"
+print(msg)
+msg = "lsdkjgnlsngl"
+print(msg)S
+msg = "My name is jirapong"
+print(msg)
+msg = "lsdkjgnlsngl"
+print(msg)S
